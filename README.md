49874583e7cc64f24087fff365eccd9b4257ed7f
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2022-05-04 01:55:52 +0800

    feat: 新增連接 mongodb connections

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # nodejs-express-week3
+
+## 此專案使用 express generator 建立
+```
+$ express --no-view --git nodejs-express-week3
+```
+## 主線任務
+- 將第二週程式碼改寫為 express 格式
+- routes： 將網址路徑管理拆到 routes 資料夾的 posts.js
+- model：將 post collections 拆到 model 資料夾，並載入到 routes/posts.js 上
--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// 1. 連接資料庫
+require('./connections');
 
-var app = express();
+// 2. 建立路由
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
