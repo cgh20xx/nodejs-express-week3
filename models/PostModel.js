5a2650b7e51fd22325d655a1b076deaf1c204b4d
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2022-05-15 15:01:22 +0800

    feat: 新增 deletePostById。設定 postModel 自帶 timestamps

--- models/PostModel.js
+++ models/PostModel.js
@@ -24,11 +24,11 @@ const postSchema = new mongoose.Schema(
       default: '',
     },
     // 若不使用內建 timestamps: true，也可自定 createAt 規則。
-    createAt: {
-      type: Date,
-      default: Date.now,
-      select: false, // false 表不顯示此欄位。Model.find() 查不出來
-    },
+    // createAt: {
+    //   type: Date,
+    //   default: Date.now,
+    //   select: false, // false 表不顯示此欄位。Model.find() 查不出來
+    // },
     content: {
       type: String,
       required: [true, 'Content 未填寫'],
@@ -43,9 +43,9 @@ const postSchema = new mongoose.Schema(
     },
   },
   {
-    // versionKey: false, // 預設都會在 document 中加上 __v: 0，若不需要，可以設定 false
+    versionKey: false, // 預設都會在 document 中加上 __v: 0，若不需要，可以設定 false
     // collection: 'posts', // 亦可直接寫死 collection 名字，不受預設小寫及結尾s影響。
-    // timestamps: true, // mongoose 會自動新增 createdAt 和 updatedAt 欄位。
+    timestamps: true, // mongoose 會自動新增 createdAt 和 updatedAt 欄位。
   }
 );
 // console.log('postSchema:', postSchema);
